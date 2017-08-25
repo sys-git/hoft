d44652bb97d3d072643ad5bd33f5326437561e2f
Francis Horsman <francis.horsman@joivy.com>
2017-08-25 15:07:37 +0100

    Version bump to 0.3

--- hoft/__version__.py
+++ hoft/__version__.py
@@ -3,9 +3,9 @@
 # pragma: no-cover
 
 __title__ = 'hoft'
-__description__ = 'Higher Order Func Tools.'
+__description__ = 'Higher Order Func Tools for Python2.7.'
 __url__ = 'https://github.com/sys-git/hoft'
-__version__ = '0.2'
+__version__ = '0.3'
 __author__ = 'Francis Horsman'
 __author_email__ = 'francis.horsman@gmail.com'
 __license__ = 'MIT'
